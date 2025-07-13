8f11dddc2af67d7f35194bc4aad469f61da44246
SADIQ ABDULRAHAMAN <40242872+Ramsoon@users.noreply.github.com>
2025-07-13 09:15:15 +0100

    Update app.js

--- Web-dep-k8-ii/app.js
+++ Web-dep-k8-ii/app.js
@@ -4,8 +4,9 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send(`
-    <h1>Hello from this NodeJS app!</h1>
+    <h1>Hello from this NodeJS app By Sadiq</h1>
     <p>Try sending a request to /error and see what happens</p>
+    <p>Note this is just a demo app</p>
   `);
 });
 
